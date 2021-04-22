dc69c5d1d04c3b6656c7f8e67d5a1e2cf7695b67
alexnaderosadc <alexanderosadc@gmail.com>
2021-04-22 23:08:20 +0300

    Resolved Critical Bug

---
+++ types.tsx
@@ -0,0 +1,31 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+  AlbumScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type Album = {
+  id: string;
+  imageUri: string;
+  artistsHeadline: string;
+}
+
+export type Song = {
+  id: string,
+  imageUri: string,
+  title: string,
+  artist: string,
+}
