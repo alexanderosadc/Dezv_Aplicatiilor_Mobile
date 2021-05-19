086d363fec0add2e3e49ed8d6b2cff6dca21a145
alexnaderosadc <alexanderosadc@gmail.com>
2021-05-19 20:36:33 +0300

    Added AWS. Data is downloading from the AWScloud Implimented GraphQL

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,26 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import SongListItem from '../components/SongListItem';
 import AlbumHeader from '../components/AlbumHeader';
 import albumDetails from '../data/albumDetails';
+import { API, graphqlOperation} from 'aws-amplify';
+import { getAlbum } from '../src/graphql/queries'
 
 const AlbumScreen = () => {
 
     const route = useRoute();
+    const albumId = route.params.id;
+
+    const [album, setAlbum] = useState(null);
 
     useEffect(() => {
+        const fetchAlbumDetails = async () =>
+        {
+            try{
+                const data = await API.graphql(graphqlOperation(getAlbum, { id:albumId }));
+                setAlbum(data.data.getAlbum);
+                console.log(data);
+            }
+            catch {
+
+            }
+        }
+
+        fetchAlbumDetails();
         console.log(route);
     }, [])
+
+    if(!album) {
+        return <Text>Loading...</Text>
+    }
 
     return (
         <View>
             <FlatList
-                data={albumDetails.songs}
+                data={album.songs.items}
                 renderItem={({ item }) => <SongListItem song={item} />}
                 keyExtractor={(item) => item.id}
-                ListHeaderComponent={() => <AlbumHeader album={albumDetails} />}
+                ListHeaderComponent={() => <AlbumHeader album={album} />}
             />
         </View>
     )
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,15 +8,36 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import AlbumScreen from "./AlbumScreen";
 
+import { API, graphqlOperation} from 'aws-amplify';
+import { listAlbumCategorys } from '../src/graphql/queries'
+import {useEffect, useState} from "react";
+
 export default function HomeScreen() {
+
+    const [categoires, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchAlbumCategories = async () => {
+            try{
+                const data = await API.graphql(graphqlOperation(listAlbumCategorys));
+                setCategories(data.data.listAlbumCategorys.items);
+                console.log(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        fetchAlbumCategories();
+    }, [])
+
     return (
         <View style={styles.container}>
             <FlatList
-                data={albumCategories}
+                data={categoires}
                 renderItem={({ item }) => (
                     <AlbumCategory
                         title={item.title}
-                        albums={item.albums}
+                        albums={item.albums.items}
                     />
                 )}
                 keyExtractor={(item) => item.id}
